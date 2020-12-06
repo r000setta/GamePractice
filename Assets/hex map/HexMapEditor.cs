c068b8abace4c657fa7fd1630c23109f35233178
r000setta <1509248091@qq.com>
2020-12-06 21:35:05 +0800

    Initial commit

---
+++ Assets/hex map/HexMapEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour
+{
+    public Color[] colors;
+    public HexGrid hexGrid;
+    private Color activeColor;
+    int activeElevation;
+    private void Awake() {
+        SelectColor(0);
+    }
+
+    private void Update() {
+        if(Input.GetMouseButton(0)&&!EventSystem.current.IsPointerOverGameObject()){
+            HandleInput();
+        }
+    }
+
+    void HandleInput(){
+        Ray inputRay=Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(inputRay,out hit)){
+            EditCell(hexGrid.GetCell(hit.point));
+        }
+    }
+
+    public void SelectColor(int idx){
+        activeColor=colors[idx];
+    }
+
+    void EditCell(HexCell cell){
+        cell.color=activeColor;
+        cell.Elevation=activeElevation;
+        hexGrid.Refresh();
+    }
+
+    public void SetElevation (float elevation) {
+		activeElevation = (int)elevation;
+	}
+}
